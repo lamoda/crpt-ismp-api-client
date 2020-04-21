07d7424c6c6c5c93e4787bf144c18c001f208558
anton.ehunov <anton.ehunov@lamoda.ru>
2020-04-21 12:17:34 +0300

    nullable for product and document description fields added

--- src/V3/Dto/FacadeDocBodyResponse/Body/DocumentDescription.php
+++ src/V3/Dto/FacadeDocBodyResponse/Body/DocumentDescription.php
@@ -7,96 +7,96 @@ namespace Lamoda\IsmpClient\V3\Dto\FacadeDocBodyResponse\Body;
 class DocumentDescription
 {
     /**
-     * @var string
+     * @var string|null
      */
     private $participantInn;
     /**
-     * @var string
+     * @var string|null
      */
     private $markingType;
     /**
-     * @var string
+     * @var string|null
      */
     private $productionDate;
     /**
-     * @var string
+     * @var string|null
      */
     private $productionType;
     /**
-     * @var string
+     * @var string|null
      */
     private $producerInn;
     /**
-     * @var string
+     * @var string|null
      */
     private $ownerInn;
 
-    public function getParticipantInn(): string
+    public function getParticipantInn(): ?string
     {
         return $this->participantInn;
     }
 
-    public function setParticipantInn(string $participantInn): self
+    public function setParticipantInn(?string $participantInn): self
     {
         $this->participantInn = $participantInn;
 
         return $this;
     }
 
-    public function getMarkingType(): string
+    public function getMarkingType(): ?string
     {
         return $this->markingType;
     }
 
-    public function setMarkingType(string $markingType): self
+    public function setMarkingType(?string $markingType): self
     {
         $this->markingType = $markingType;
 
         return $this;
     }
 
-    public function getProductionDate(): string
+    public function getProductionDate(): ?string
     {
         return $this->productionDate;
     }
 
-    public function setProductionDate(string $productionDate): self
+    public function setProductionDate(?string $productionDate): self
     {
         $this->productionDate = $productionDate;
 
         return $this;
     }
 
-    public function getProductionType(): string
+    public function getProductionType(): ?string
     {
         return $this->productionType;
     }
 
-    public function setProductionType(string $productionType): self
+    public function setProductionType(?string $productionType): self
     {
         $this->productionType = $productionType;
 
         return $this;
     }
 
-    public function getProducerInn(): string
+    public function getProducerInn(): ?string
     {
         return $this->producerInn;
     }
 
-    public function setProducerInn(string $producerInn): self
+    public function setProducerInn(?string $producerInn): self
     {
         $this->producerInn = $producerInn;
 
         return $this;
     }
 
-    public function getOwnerInn(): string
+    public function getOwnerInn(): ?string
     {
         return $this->ownerInn;
     }
 
-    public function setOwnerInn(string $ownerInn): self
+    public function setOwnerInn(?string $ownerInn): self
     {
         $this->ownerInn = $ownerInn;
 
